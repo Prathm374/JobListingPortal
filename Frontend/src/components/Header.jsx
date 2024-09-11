ab6f43761de67e0124dcff727617ba1a1269c6fc
Prathmesh Gahukar <prathmeshgahukar001@gmail.com>
2024-09-11 22:46:26 +0530

    third

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -6,23 +6,43 @@ import { NavLink, useNavigate } from "react-router-dom";
 export default function LoginNav() {
     return (
         <header className="p-3 mb-3 border-bottom">
-        <div className="container">
-          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
-              <img src={logo} alt="" className="bi me-2" width="40" height="32" role="img"/>
-            </a>
-    
-            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-              <li><a href="#" className="nav-link px-2 link-secondary">Overview</a></li>
-              <li><a href="#" className="nav-link px-2 link-body-emphasis">Inventory</a></li>
-              <li><a href="#" className="nav-link px-2 link-body-emphasis">Customers</a></li>
-              <li><a href="#" className="nav-link px-2 link-body-emphasis">Products</a></li>
-            </ul>
-    
-            <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <div className="container">
+                <div className="mpnav">
+                    <div className="logNav">
+                        <div className="logNavLogo">
+                            <NavLink to="/home">
+                                <img src={logo} alt="logo" />
+                            </NavLink>
+                        </div>
+                        <p>JobConnect</p>
+                        <div className="navItems">
+                            <NavLink to="/home">
+                                <p>Jobs</p>
+                            </NavLink>
+                            <NavLink to="/home">
+                                <p>Profile</p>
+                            </NavLink>
+                            <NavLink to="/home">
+                                <p>Dashboard</p>
+                            </NavLink>
+                            <NavLink to="/home">
+                                <p>About us</p>
+                            </NavLink>
+                            <NavLink to="/home">
+                                <p>Support</p>
+                            </NavLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+{
+    /* <form className="col-12 col-lg-2 mb-3 mb-lg-0 me-lg-3" role="search">
               <input type="search" className="form-control" placeholder="Search..." aria-label="Search" />
             </form>
-    
             <div className="dropdown text-end">
               <a href="#" className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                 <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle"/>
@@ -34,9 +54,5 @@ export default function LoginNav() {
                 <li><hr className="dropdown-divider"/></li>
                 <li><a className="dropdown-item" href="#">Sign out</a></li>
               </ul>
-            </div>
-          </div>
-        </div>
-      </header>
-    );
+            </div> */
 }
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,37 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
-import { toast, ToastContainer } from 'react-toastify';
-import axios from "axios"
+import { toast, ToastContainer } from "react-toastify";
+import axios from "axios";
 
-import Navbar from '../components/LogNav'
-import Footer from '../components/Footer'
+import Navbar from "../components/LogNav";
+import Footer from "../components/Footer";
 
-import './Login.css'
-import 'react-toastify/dist/ReactToastify.css';
+import "./Login.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Login() {
-  const [username, setUsername] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleGetStarted = () => {
-    navigate("/signup");
-  };
+    const handleGetStarted = () => {
+        navigate("/signup");
+    };
 
-  const handleScrollDown = () => {
-    window.scrollBy({
-      top: window.innerHeight,
-      behavior: "smooth",
-    });
-  };
+    const handleScrollDown = () => {
+        window.scrollBy({
+            top: window.innerHeight,
+            behavior: "smooth",
+        });
+    };
 
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post("http://localhost:5000/login", { username, password });
+            const response = await axios.post("http://localhost:5000/login", {
+                username,
+                password,
+            });
             if (response.data.success) {
                 toast.success("Login Success.");
-                navigate("/home");                
+                navigate("/home");
             } else {
                 toast.error("Invalid username or password. Please try again.");
             }
@@ -41,110 +44,135 @@ export default function Login() {
         }
     };
 
-  return (
-    <div className='loginpg'>
-      <Navbar />
-      <div>
-      <div className="container col-xxl-8 px-4 py-5">
-        <div className="row flex-lg-row align-items-center g-5 py-5">
-          <div className="col-lg-6">
-            <h1 className="display-5 fw-bold text-info lh-1 mb-3">
-            Unlock Your Next Opportunity with JobConnect
-            </h1>
-            <p className="lead text-light">
-            Find jobs that match your skills and aspirations. With real-time listings and easy applications, we connect you to employers who need your talent.
-            </p>
-            <p className="lead text-light">
-            Start your career journey today and land your dream job faster.
-            </p>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-              <button
-                onClick={handleGetStarted}
-                type="button"
-                className="cssbuttons-io-button" >
-                Get started
-                <div class="icon">
-                  <svg
-                    height="24"
-                    width="24"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M0 0h24v24H0z" fill="none"></path>
-                    <path
-                      d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
-                </div>
-              </button>
-              <button
-                onClick={handleScrollDown}
-                type="button"
-                className="cssbuttons-io-button btn2"
-              >
-                Learn More
-                <div class="icon">
-                  <svg
-                    height="24"
-                    width="24"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M0 0h24v24H0z" fill="none"></path>
-                    <path
-                      d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
-                </div>
-              </button>
-            </div>
-          </div>
-          <div className="col-10 col-sm-8 col-lg-6 heroLogin">
-            <div className="loginForm">
-              <div className="wrap">
-            <form onSubmit={handleLogin}>
-                <h1 className="text-dark">Login</h1>
-                <div className="input-box">
-                    <input 
-                        type="text" 
-                        placeholder="Username" 
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required 
-                    />
-                    <i className='bx bxs-user'></i>
-                </div>
+    return (
+        <div className="loginpg">
+            <Navbar />
+            <div className="container col-xxl-10 px-4 py-5 loginHero">
+                <div className="row flex-lg-row align-items-center g-5 py-5">
+                    <div className="col-lg-7">
+                        <h1 className="display-4 fw-bold lh-2 mb-3">
+                            Unlock Your Next Opportunity with <p className="text-info">JobConnect</p>
+                        </h1>
+                        <p className="lead">
+                            Find jobs that match your skills and aspirations.
+                            With <mark><em>real-time listings</em></mark><br />and <mark><em>easy applications</em></mark>, we
+                            connect you to employers who need your talent.
+                        </p>
+                        <p className="lead fw-bold">
+                            Start your career journey today and land your dream
+                            job faster.
+                        </p>
+                        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                            <button
+                                onClick={handleGetStarted}
+                                type="button"
+                                className="cssbuttons-io-button"
+                            >
+                                Get started
+                                <div class="icon">
+                                    <svg
+                                        height="24"
+                                        width="24"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M0 0h24v24H0z"
+                                            fill="none"
+                                        ></path>
+                                        <path
+                                            d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                                            fill="currentColor"
+                                        ></path>
+                                    </svg>
+                                </div>
+                            </button>
+                            <button
+                                onClick={handleScrollDown}
+                                type="button"
+                                className="cssbuttons-io-button btn2"
+                            >
+                                Learn More
+                                <div class="icon">
+                                    <svg
+                                        height="24"
+                                        width="24"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M0 0h24v24H0z"
+                                            fill="none"
+                                        ></path>
+                                        <path
+                                            d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                                            fill="currentColor"
+                                        ></path>
+                                    </svg>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="col-10 col-sm-8 col-lg-5 heroLogin">
+                         <div className="loginForm">
+                            <div className="wrap">
+                                <form onSubmit={handleLogin}>
+                                    <h1 className="text-dark">Login</h1>
+                                    <div className="input-box">
+                                        <input
+                                            type="text"
+                                            placeholder="Username"
+                                            value={username}
+                                            onChange={(e) =>
+                                                setUsername(e.target.value)
+                                            }
+                                            required
+                                        />
+                                        <i className="bx bxs-user"></i>
+                                    </div>
 
-                <div className="input-box">
-                    <input 
-                        type="password" 
-                        placeholder="Password" 
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required 
-                    />
-                    <i className='bx bxs-lock-alt'></i>
+                                    <div className="input-box">
+                                        <input
+                                            type="password"
+                                            placeholder="Password"
+                                            value={password}
+                                            onChange={(e) =>
+                                                setPassword(e.target.value)
+                                            }
+                                            required
+                                        />
+                                        <i className="bx bxs-lock-alt"></i>
+                                    </div>
+                                    <div className="remember-forget">
+                                        <label style={{ color: "#54D1C9" }}>
+                                            <input type="checkbox" /> Remember
+                                            Me{" "}
+                                        </label>
+                                        <a href="/">Forgot Password?</a>
+                                    </div>
+                                    
+                                    <div className="register-link">
+                                        <p>
+                                            Don't have an account?{" "}
+                                            <NavLink to={"/signup"}>
+                                                Register
+                                            </NavLink>
+                                        </p>
+                                    </div>
+                                    <button
+                                        type="submit"
+                                        className="btn btn-info btn1"
+                                    >
+                                        Login
+                                    </button>
+                                </form>
+                                <ToastContainer />
+                            </div>
+                        </div> 
+                    </div>
                 </div>
-                <div className="remember-forget">
-                    <label style={{color: "#54D1C9"}}><input type="checkbox" /> Remember Me </label>
-                    <a href="/">Forgot Password?</a>
-                </div>
-                {/* Add Google oAuth Here:  */}
-                <div className="register-link">
-                    <p>Don't have an account? <NavLink to={"/signup"}>Register</NavLink></p>
-                </div>
-                <button type="submit" className="btn btn-info btn1">Login</button>
-            </form>
-            <ToastContainer />
-        </div>
             </div>
-          </div>
+            <Footer />
         </div>
-      </div>
-    </div>
-      <Footer />
-    </div>
-  )
+    );
 }
